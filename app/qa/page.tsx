495d76953bd3fc4a196f831c68808257826d9f58
Sezer Tanrıverdioğlu <sezertanriverdi@gmail.com>
2023-07-25 09:13:45 +0300

    Create and Join Session

--- app/qa/page.tsx
+++ app/qa/page.tsx
@@ -1,10 +1,38 @@
-import { PencilIcon } from "@heroicons/react/24/solid";
+"use client";
+
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Head from "next/head";
+import { useState } from "react";
 
 import Button from "@/src/components/common/button/Button";
 import Input from "@/src/components/common/input/Input";
+import { Database } from "@/supabase/database";
 
 export default function QuestionsAnswers() {
+  const supabase = useSupabaseClient<Database>();
+  const [sessionName, setSessionName] = useState<string>("");
+  async function createSession() {
+    // @ts-ignore
+    const { data } = await supabase.rpc("max_session_track_id", {});
+    // @ts-ignore
+    const maxSessionTrackId = parseInt(data) + 1;
+
+    const { error } = await supabase.from("qa_sessions").insert({
+      session_name: `${sessionName}`,
+      session_track_id: String(maxSessionTrackId)
+    });
+
+    if (error) {
+      alert("Error creating session");
+    }
+
+    window.location.href = `/qa/${maxSessionTrackId}`;
+  }
+
+  function joinSession() {
+    window.location.href = `/qa/${sessionName}`;
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -19,13 +47,19 @@ export default function QuestionsAnswers() {
               label="Session Name"
               name="session_name"
               type={"text"}
+              value={sessionName}
+              onChange={e => setSessionName(e.target.value)}
               placeholder="Enter Session Name"
               maxLength={50}
             />
-            <div className="mt-4 flex justify-center items-center">
-              <Button className="w-full">Create</Button>
+            <div className="mt-4 flex items-center justify-center">
+              <Button className="w-full" onClick={() => createSession()}>
+                Create
+              </Button>
               <p className="mx-4 text-gray-500">or</p>
-              <Button className="w-full">Join</Button>
+              <Button className="w-full" onClick={() => joinSession()}>
+                Join
+              </Button>
             </div>
           </div>
         </div>
