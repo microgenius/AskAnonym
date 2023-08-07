11a6d5f837fab24da5543b8927374023d72117ac
Sezer Tanrıverdioğlu <sezertanriverdi@gmail.com>
2023-08-07 16:52:14 +0300

    Logo and title has been changed

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -10,13 +10,13 @@ interface ILogo {
 function Logo({ width = 100, height = 40 }: ILogo) {
   return (
     <>
-      <Link href="/" className="flex justify-start">
-        {/* <div className="text-3xl font-extrabold  text-purple-700 dark:text-purple-400 ">
-          <span className="text-4xl">ask</span>
-          <span className="text-2xl text-yellow-400"> anonym</span>
-          <span>¿</span>
-        </div> */}
-        <Image src="/images/logo.png" alt="cta" width={width} height={height} />
+      <Link href="/" className="ml-6 justify-start">
+        <Image
+          src="/images/logo-yg.png"
+          alt="cta"
+          width={width}
+          height={height}
+        />
       </Link>
     </>
   );
